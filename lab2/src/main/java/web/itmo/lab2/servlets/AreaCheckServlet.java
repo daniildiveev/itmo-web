6dea2fa148a9e0c8a08422f4f8404a764ac55f10
Daniil Diveev <daniildiveev@mail.ru>
2023-11-19 22:03:21 +0300

    [MODIFY] servlets done, added error handling and attributes storage in request

--- lab2/src/main/java/web/itmo/lab2/servlets/AreaCheckServlet.java
+++ lab2/src/main/java/web/itmo/lab2/servlets/AreaCheckServlet.java
@@ -1,11 +1,14 @@
 package web.itmo.lab2.servlets;
 
 import java.io.*;
+import java.util.ArrayList;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import web.itmo.lab2.checker.HitChecker;
+import web.itmo.lab2.models.HitDataBean;
+import web.itmo.lab2.models.HitDataTableBean;
 import web.itmo.lab2.validator.ParameterValidator;
 import web.itmo.lab2.validator.ValidParameter;
 
@@ -15,62 +18,57 @@ public class AreaCheckServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response)
             throws IOException, ServletException {
-        String xString = request.getParameter("x");
-        String yString = request.getParameter("y");
-        String rString = request.getParameter("r");
+        try {
+            String xString = request.getParameter("x");
+            String yString = request.getParameter("y");
+            String rString = request.getParameter("r");
 
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter out = response.getWriter();
-        String message = "";
+            response.setCharacterEncoding("UTF-8");
+            String message = "";
 
-        ValidParameter checkString = ParameterValidator.checkStrings(xString, yString, rString);
+            ValidParameter checkString = ParameterValidator.checkStrings(xString, yString, rString);
 
-        if (checkString.getValid()) {
-            float x = Float.parseFloat(xString);
-            float y = Float.parseFloat(yString);
-            float R = Float.parseFloat(rString);
+            if (checkString.getValid()) {
+                float x = Float.parseFloat(xString);
+                float y = Float.parseFloat(yString);
+                float R = Float.parseFloat(rString);
 
-            ValidParameter checkRanges = ParameterValidator.checkRanges(x, y, R);
+                ValidParameter checkRanges = ParameterValidator.checkRanges(x, y, R);
 
-            if (checkRanges.getValid()) {
-                boolean hit = HitChecker.checkHit(x, y, R);
+                if (checkRanges.getValid()) {
+                    boolean hit = HitChecker.checkHit(x, y, R);
 
-                if (hit) {
-                    message = "Point hits the area";
+                    if (hit) {
+                        message = "Point hits the area";
+                    } else {
+                        message = "Point doesn't hit the area";
+                    }
                 } else {
-                    message = "Point doesn't hit the area";
+                    message = checkRanges.getMessage();
                 }
             } else {
-                message = checkRanges.getMessage();
+                message = checkString.getMessage();
             }
-        } else {
-            message = checkString.getMessage();
+
+            HitDataBean newBean = new HitDataBean();
+
+            newBean.setX(xString);
+            newBean.setY(yString);
+            newBean.setR(rString);
+            newBean.setHit(message);
+
+            HitDataTableBean hitTable = (HitDataTableBean) request.getAttribute("resultTable");
+            ArrayList<HitDataBean> beans = hitTable.getHits();
+
+            beans.add(newBean);
+            hitTable.setHits(beans);
+
+            getServletContext().setAttribute("resultRow", newBean);
+            getServletContext().setAttribute("resultTable", hitTable);
         }
-
-        HitDataBean newBean = new HitDataBean();
-
-        newBean.setX(xString);
-        newBean.setY(yString);
-        newBean.setR(rString);
-        newBean.setHit(message);
-
-        hits.add(newBean);
-
-        request.setAttribute("beanList", hits);
-
-        String outputHtml = "<DOCTYPE html>" +
-                            "<html><body>" +
-                            "<table>" +
-                            "<thead><tr><th>X</th><th>Y</th><th>R</th></tr></thead>" +
-                            "<tbody><tr><td>" + xString + "</td><td>" + yString +
-                            "</td><td>" + rString + "</td><td>" + message + "</td></tr></tbody>" +
-                            "</table>" +
-                            "</body></html>";
-
-        out.write(outputHtml);
-
-        out.flush();
-        out.close();
+        catch (Exception e) {
+            getServletContext().setAttribute("error", e.getMessage());
+            getServletContext().getRequestDispatcher("/error.jsp").forward(request, response);
+        }
     }
 }
--- lab2/src/main/java/web/itmo/lab2/servlets/ControllerServlet.java
+++ lab2/src/main/java/web/itmo/lab2/servlets/ControllerServlet.java
@@ -3,15 +3,23 @@ package web.itmo.lab2.servlets;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import web.itmo.lab2.models.HitDataTableBean;
 
 import java.io.IOException;
 
 @WebServlet(name = "controllerServet", value = "/controller")
 public class ControllerServlet extends HttpServlet{
+    private HitDataTableBean beans;
+
+    public ControllerServlet() {
+        this.beans = new HitDataTableBean();
+    }
+
     @Override
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response)
             throws ServletException, IOException {
+        request.setAttribute("resultTable", this.beans);
         processRequest(request, response);
     }
 
@@ -19,22 +27,28 @@ public class ControllerServlet extends HttpServlet{
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response)
             throws ServletException, IOException{
+        request.setAttribute("resultTable", this.beans);
         processRequest(request, response);
     }
 
     private void processRequest(HttpServletRequest request,
                                 HttpServletResponse response)
             throws ServletException, IOException{
-        String x = request.getParameter("x");
-        String y = request.getParameter("y");
-        String R = request.getParameter("r");
+        try {
+            String x = request.getParameter("x");
+            String y = request.getParameter("y");
+            String R = request.getParameter("r");
 
-        boolean hasCoordinates = (x != null ) && (y != null) && (R != null);
+            boolean hasCoordinates = (x != null) && (y != null) && (R != null);
 
-        if (hasCoordinates) {
-            request.getRequestDispatcher("/area-check-servlet").forward(request, response);
-        } else {
-            request.getRequestDispatcher("/index.jsp").forward(request, response);
+            if (hasCoordinates) {
+                request.getRequestDispatcher("/area-check-servlet").forward(request, response);
+            } else {
+                request.getRequestDispatcher("/index.jsp").forward(request, response);
+            }
+        } catch (Exception e){
+            getServletContext().setAttribute("error", e.getMessage());
+            getServletContext().getRequestDispatcher("/error.jsp").forward(request, response);
         }
     }
 }
