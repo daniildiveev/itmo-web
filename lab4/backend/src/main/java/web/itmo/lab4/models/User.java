aac1fa8f9895655a030238dbf136c2a26964779b
Daniil Diveev <daniildiveev@mail.ru>
2024-01-09 02:54:12 +0300

    fix: rearrange the backend dirs

---
+++ lab4/backend/src/main/java/web/itmo/lab4/models/User.java
@@ -0,0 +1,44 @@
+package web.itmo.lab4.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@Table
+@Entity(name="users")
+public class User implements UserDetails {
+    @Id
+    private String username;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
