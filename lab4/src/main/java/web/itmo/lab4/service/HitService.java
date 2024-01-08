8365c3a5150d5fd8202ccb6b38de15865e18eb34
Daniil Diveev <daniildiveev@mail.ru>
2024-01-09 02:56:40 +0300

    rm: delete previous backend files

--- lab4/src/main/java/web/itmo/lab4/service/HitService.java
+++
@@ -1,7 +0,0 @@
-package web.itmo.lab4.service;
-
-import web.itmo.lab4.models.Hit;
-
-public interface HitService {
-    Hit addHit(Hit hit);
-}
--- lab4/src/main/java/web/itmo/lab4/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package web.itmo.lab4.service;
-
-import web.itmo.lab4.models.User;
-
-public interface UserService {
-    boolean authenticate(User user);
-    boolean register(User user);
-}
