1155e30ff49a75fa0a6a900401ae8b21b06fa7c5
Daniil Diveev <daniildiveev@mail.ru>
2024-01-08 03:15:19 +0300

    feat: implement JWT logic

---
+++ lab4/src/main/java/web/itmo/lab4/filters/JWTFilter.java
@@ -0,0 +1,58 @@
+package web.itmo.lab4.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import web.itmo.lab4.service.JWTService;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+    private final JWTService jwtService;
+    private final UserDetailsService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        final String token = request.getHeader("Authorization");
+
+        if (!token.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+
+            return ;
+        }
+
+        final String jwt = token.substring(7);
+        final String username = jwtService.extractUsername(jwt);
+
+        if (username == null) {
+            filterChain.doFilter(request, response);
+
+            return ;
+        }
+
+        UserDetails details = userService.loadUserByUsername(username);
+
+        if (jwtService.validToken(jwt, details)) {
+            UsernamePasswordAuthenticationToken pezdez = new UsernamePasswordAuthenticationToken(details, null, details.getAuthorities());
+
+            pezdez.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(pezdez);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
